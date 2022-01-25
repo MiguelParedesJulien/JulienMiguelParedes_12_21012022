784990a88e8fd6270ad775bf279851178bbd0697
Miguel Paredes Julien <julien314@hotmail.fr>
2022-01-25 23:42:44 +0100

    Ajout du composant HomeCard qui permet un affichage des users sur la page home

---
+++ sportsee/src/components/leftSide/LeftSide.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import yoga from "../../assets/img/yoga.svg"
+import natation from "../../assets/img/natation.svg"
+import velo from "../../assets/img/velo.svg"
+import haltere from "../../assets/img/haltere.svg"
+import "./leftSide.css";
+
+/**
+ * React Component displaying the left side bar
+ * @returns {ReactElement}
+ */
+
+ function LeftSide() {
+    return (
+      <aside>
+        <ul className="aside-ul">
+          <li className="icone-bar">
+            <img src={yoga} alt="yoga" />
+          </li>
+          <li className="icone-bar">
+            <img src={natation} alt="natation" />
+          </li>
+          <li className="icone-bar">
+            <img src={velo} alt="velo" />
+          </li>
+          <li className="icone-bar">
+            <img src={haltere} alt="haltere" />
+          </li>
+        </ul>
+        <div>
+          <span className="copyright">Copyright, SportSee 2020</span>
+        </div>
+      </aside>
+    );
+  }
+  
+  export default LeftSide;
--- sportsee/src/pages/home/Home.jsx
+++ sportsee/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/header/Header";
+import HomeCard from "../../components/homeCard/HomeCard";
 
 function Home() {
   return (
     <div>
       <Header />
+      <HomeCard />
     </div>
   );
 }
