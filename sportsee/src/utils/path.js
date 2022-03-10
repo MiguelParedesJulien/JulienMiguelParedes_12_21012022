ad249826bbf56ef367baa4e51c2026459c8cb75b
Miguel Paredes Julien <julien314@hotmail.fr>
2022-03-10 16:09:33 +0100

    Changement port application

--- sportsee/src/utils/path.js
+++ sportsee/src/utils/path.js
@@ -1,39 +1,39 @@
-const pathMocked = {
-  url: {
-    infoUser(userId) {
-      return `../../data/${userId}/userInfo.json`;
-    },
-    userAverageSession(userId) {
-      return `../../data/${userId}/userAverageSessions.json`;
-    },
-    userActivity(userId) {
-      return `../../data/${userId}/userActivity.json`;
-    },
-    userPerformance(userId) {
-      return `../../data/${userId}/userPerformance.json`;
-    },
-  },
-};
-
-// const pathApi = {
+// const pathMocked = {
 //   url: {
 //     infoUser(userId) {
-//       return `http://localhost:3000/user/${userId}`;
+//       return `../../data/${userId}/userInfo.json`;
 //     },
 //     userAverageSession(userId) {
-//       return `http://localhost:3000/user/${userId}/average-sessions`;
+//       return `../../data/${userId}/userAverageSessions.json`;
 //     },
 //     userActivity(userId) {
-//       return `http://localhost:3000/user/${userId}/activity`;
+//       return `../../data/${userId}/userActivity.json`;
 //     },
 //     userPerformance(userId) {
-//       return `http://localhost:3000/user/${userId}/performance`;
+//       return `../../data/${userId}/userPerformance.json`;
 //     },
 //   },
 // };
+
+const pathApi = {
+  url: {
+    infoUser(userId) {
+      return `http://localhost:3000/user/${userId}`;
+    },
+    userAverageSession(userId) {
+      return `http://localhost:3000/user/${userId}/average-sessions`;
+    },
+    userActivity(userId) {
+      return `http://localhost:3000/user/${userId}/activity`;
+    },
+    userPerformance(userId) {
+      return `http://localhost:3000/user/${userId}/performance`;
+    },
+  },
+};
 
 /**
  * when deploying the API, change the path to the corresponding url
  * @type { string }
  */
-export const path = pathMocked;
+export const path = pathApi;
